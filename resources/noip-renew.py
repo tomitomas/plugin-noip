f9fd0c87c066e800b4912506973b037a27404587
hugoKs3 <hugo@questroy.com>
2021-04-09 12:49:53 +0200

    fix login issue

--- resources/noip-renew.py
+++ resources/noip-renew.py
@@ -80,6 +80,7 @@ class Robot:
         #ele_pwd.send_keys(base64.b64decode(self.password).decode('utf-8'))
         ele_pwd.send_keys(self.password)
         self.browser.find_element_by_name("Login").click()
+        self.find_element_by_xpath('//button[@data-action="login"]').click()
         time.sleep(3)
         if self.debug > 1:
             self.browser.save_screenshot(self.rootpath + "/data/debug2.png")
